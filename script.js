323d18151b7f8a211ab4dcdfb031d506f44b2a3e
BogdanGHUB <poprik.bogdan2014@yandex.ru>
2020-11-26 21:07:05 +0400

    Добавление генератора фона

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-		var quotes = [
- 		"Стажировка",
- 		"Q-Digital",
- 		"Выполнил Богдан Поприк",
- 		"Код комментировал",
- 		"Вроде получилось неплохо",
- 		"Изучаем JavaScript",
- 		"CSS",
- 		"HTML",
- 		"Познал GitHub",
- 		"Залил на GitHub"
- 		]
+var quotes = [
+ 	"Стажировка.",
+ 	"Q-Digital.",
+ 	"Выполнил Богдан Поприк.",
+ 	"Код комментировал.",
+ 	"Вроде получилось неплохо.",
+ 	"Изучаем JavaScript.",
+ 	"CSS.",
+ 	"HTML.",
+ 	"Познал GitHub.",
+ 	"Залил на GitHub."
+ ]
 
-function newQuote()
+	function newQuote()
 {
 	var randomNumber = Math.floor(Math.random()*(quotes.length));
 	document.getElementById('text').innerHTML = quotes[randomNumber];
+}
+
+var color = [
+ 	"color1",
+ 	"color2",
+ 	"color3",
+ 	"color4",
+ 	"color5",
+ 	"color6",
+ 	"color7",
+ 	"color8",
+ ]
+ 
+function colorbg()
+{
+	var randomcolor = Math.floor(Math.random()*(color.length));
+	document.getElementById('wrapper').className = color[randomcolor];
 }
 
