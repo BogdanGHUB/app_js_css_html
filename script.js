8ab57ed05c33d1c073566c7f2f3da4e18eb676b5
BogdanGHUB <poprik.bogdan2014@yandex.ru>
2020-11-30 20:58:22 +0400

    New Changes

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
-//Рандомный текст
+//ПЕРЕМЕННЫЕ
+let generate = document.getElementById('generate_text_bg');
+let clear = document.getElementById('clear');
+let color_wrapper = document.getElementById('wrapper');
+let text = document.getElementById("text");
 
-var text = [
- 	"Стажировка.",
- 	"Q-Digital.",
- 	"Выполнил Богдан Поприк.",
- 	"Код комментировал.",
- 	"Вроде получилось неплохо.",
- 	"Изучаем JavaScript.",
- 	"CSS.",
- 	"HTML.",
- 	"Познал GitHub.",
- 	"Залил на GitHub."
- ]
+//МАССИВЫ
 
-	function randomText()
+let textRandom = [
+   "Стажировка.",
+   "Q-Digital.",
+   "Выполнил Богдан Поприк.",
+   "Код комментировал.",
+   "Вроде получилось неплохо.",
+   "Изучаем JavaScript.",
+   "CSS.",
+   "HTML.",
+   "Познал GitHub.",
+   "Залил на GitHub."
+];
+
+let randomColorBgText = [
+   "color1",
+   "color2",
+   "color3",
+   "color4",
+   "color5",
+   "color6"
+];
+
+//ГЕНЕРАЦИЯ РАНДОМНОГО ЦВЕТА ФОНА И ТЕКСТА
+
+Array.prototype.randomColor = function ()
 {
-	var randomNumber = Math.floor(Math.random()*(text.length));
-	document.getElementById('text').innerHTML = text[randomNumber];
-}
+   return this[Math.floor(Math.random() * this.length)]
+};
 
-//Рандомный цвет фона
-
-var color = [
- 	"color1",
- 	"color2",
- 	"color3",
- 	"color4",
- 	"color5",
- 	"color6"
- ]
-
-function colorbg()
+Array.prototype.randomText = function () 
 {
-	var randomcolor = Math.floor(Math.random()*(color.length));
-	document.getElementById('wrapper').className = color[randomcolor];
-}
+   return this[Math.floor(Math.random() * this.length)]
+};
 
-// Очистить текст (получилось не так как нужно по заданию)
+generate.addEventListener('click', function () 
+{
+   let TwoRandomColor = randomColorBgText.randomColor();
+   let TwoRandomText = textRandom.randomText();
 
-	function reset()
-{	
-	let paragraph = document.getElementById('text');
-	paragraph.textContent = '';
-}
+   color_wrapper.classList.remove("color1", "color2", "color3", "color4", "color5", "color6");
+   color_wrapper.classList.add(TwoRandomColor);
+   text.classList.remove("color1", "color2", "color3", "color4", "color5", "color6");
+   text.classList.add(TwoRandomColor);
 
-// Очистить фон не получилось
+   text.innerHTML = TwoRandomText;
+   text.insertAdjacentElement('afterbegin', text);
+});
+
+//УДАЛЕНИЕ КЛАССОВ ТЕКСТА И ФОНА
+
+clear.addEventListener('click', function ()
+{
+   color_wrapper.classList.remove("color1", "color2", "color3", "color4", "color5", "color6");
+   text.innerHTML = '';
+})
